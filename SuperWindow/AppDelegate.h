5a0d2ba44558062a6ffe60fe9cdcbbd7f7f3f097
Jasonbroker <zhiye1@hotmail.com>
2017-05-22 21:00:34 +0800

    init

---
+++ SuperWindow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SuperWindow
+//
+//  Created by Jason on 22/05/2017.
+//  Copyright © 2017 51talk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
