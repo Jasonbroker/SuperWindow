1cd8e827189edf652bec6521c8623cddb531e25a
Jasonbroker <zhiye1@hotmail.com>
2017-06-04 19:28:00 +0800

    collection view

--- SuperWindow/SuperWindow/DebugViewController.m
+++ SuperWindow/SuperWindow/DebugViewController.m
@@ -8,6 +8,8 @@
 
 #import "DebugViewController.h"
 
+static NSString *const cellId = @"cellId";
+
 @interface DebugCollectionViewFlowLayout : UICollectionViewFlowLayout
 
 @end
@@ -16,7 +18,7 @@
 
 @end
 
-@interface DebugCell : UITableViewCell
+@interface DebugCell : UICollectionViewCell
 
 @end
 
@@ -24,7 +26,13 @@
 
 @end
 
-@interface DebugViewController ()
+@interface DebugViewController ()<UICollectionViewDelegate, UICollectionViewDataSource>
+
+@property (nonatomic, strong)DebugCollectionViewFlowLayout *layout;
+
+@property (nonatomic, strong)UICollectionView *collectionView;
+
+@property (nonatomic, strong)NSArray *dataArray;
 
 @end
 
@@ -33,22 +41,110 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    self.view.backgroundColor = [UIColor grayColor];
+    self.layout = [[DebugCollectionViewFlowLayout alloc] init];
+    self.collectionView = [[UICollectionView alloc] initWithFrame:self.view.frame collectionViewLayout:self.layout];
+    self.collectionView.dataSource = self;
+    self.collectionView.delegate = self;
+    self.collectionView.backgroundColor = [UIColor lightTextColor];
+    [self.view addSubview:self.collectionView];
+    
+    [self.collectionView registerClass:[DebugCell class] forCellWithReuseIdentifier:cellId];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark ---- UICollectionViewDataSource
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
+    return 1;
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return self.dataArray.count;
 }
-*/
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    DebugCell *cell = [_collectionView dequeueReusableCellWithReuseIdentifier:cellId forIndexPath:indexPath];
+    cell.backgroundColor = [UIColor purpleColor];
+    return cell;
+}
+
+- (BOOL)collectionView:(UICollectionView *)collectionView canMoveItemAtIndexPath:(NSIndexPath *)indexPath {
+    return YES;
+}
+
+
+- (void)collectionView:(UICollectionView *)collectionView moveItemAtIndexPath:(NSIndexPath *)sourceIndexPath toIndexPath:(NSIndexPath*)destinationIndexPath{
+}
+
+#pragma mark ---- UICollectionViewDelegateFlowLayout
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
+    return CGSizeMake(150, 150);
+}
+
+- (UIEdgeInsets)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout insetForSectionAtIndex:(NSInteger)section {
+    return UIEdgeInsetsMake(5, 5, 5, 5);
+}
+
+- (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout minimumLineSpacingForSectionAtIndex:(NSInteger)section {
+    return 5.f;
+}
+
+- (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout minimumInteritemSpacingForSectionAtIndex:(NSInteger)section {
+    return 5.f;
+}
+
+#pragma mark ---- UICollectionViewDelegate
+- (BOOL)collectionView:(UICollectionView *)collectionView shouldHighlightItemAtIndexPath:(NSIndexPath *)indexPath {
+    return YES;
+}
+
+// 点击高亮
+- (void)collectionView:(UICollectionView *)collectionView didHighlightItemAtIndexPath:(NSIndexPath *)indexPath {
+    UICollectionViewCell *cell = [collectionView cellForItemAtIndexPath:indexPath];
+    cell.backgroundColor = [UIColor greenColor];
+}
+
+// 选中某item
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath {
+    
+}
+
+
+// 长按某item，弹出copy和paste的菜单
+- (BOOL)collectionView:(UICollectionView *)collectionView shouldShowMenuForItemAtIndexPath:(NSIndexPath *)indexPath {
+    return YES;
+}
+
+// 使copy和paste有效
+- (BOOL)collectionView:(UICollectionView *)collectionView canPerformAction:(SEL)action forItemAtIndexPath:(NSIndexPath *)indexPath withSender:(nullable id)sender {
+    if ([NSStringFromSelector(action) isEqualToString:@"copy:"] || [NSStringFromSelector(action) isEqualToString:@"paste:"])
+    {
+        return YES;
+    }
+    
+    return NO;
+}
+
+//
+- (void)collectionView:(UICollectionView *)collectionView performAction:(SEL)action forItemAtIndexPath:(NSIndexPath *)indexPath withSender:(nullable id)sender {
+    if([NSStringFromSelector(action) isEqualToString:@"copy:"])
+    {
+        //        NSLog(@"-------------执行拷贝-------------");
+        [_collectionView performBatchUpdates:^{
+            // [self.dataArray removeObjectAtIndex:indexPath.row];
+            [_collectionView deleteItemsAtIndexPaths:@[indexPath]];
+        } completion:nil];
+    }
+    else if([NSStringFromSelector(action) isEqualToString:@"paste:"])
+    {
+        NSLog(@"-------------执行粘贴-------------");
+    }
+}
+
+#pragma mark -setter getter
+- (NSArray *)dataArray {
+    if (!_dataArray) {
+        _dataArray = @[@"重置APP", @"清除浏览器缓存", @"清除UserDefault", @"清除所有缓存",@"清除浏览器缓存",@"文件浏览器",@"自定义功能"];
+    }
+    return _dataArray;
+}
 
 @end
